3155af5f3ec10c5bd10b95d46706ecd35c9028e8
Jromanosorio <jandres.roman.08@gmail.com>
2025-11-02 01:03:32 -0500

    database config

---
+++ backend/src/database/config.ts
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+const sequelizeConfig = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT || '3306'),
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD || '',
+    database: process.env.DATABASE_NAME || 'testDB',
+})
+
+const testConnection = async () => {
+  try {
+    await sequelizeConfig.authenticate();
+    console.log('Database connection established successfully');
+    return true;
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    return false;
+  }
+};
+
+export { 
+  sequelizeConfig, 
+  testConnection
+}
