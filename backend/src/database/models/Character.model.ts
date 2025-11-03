f4d3dbe6d8627444f2b4861d5cf76efbb8ca9487
Jromanosorio <jandres.roman.08@gmail.com>
2025-11-03 12:31:16 -0500

    character model updated

--- backend/src/database/models/Character.model.ts
+++ backend/src/database/models/Character.model.ts
@@ -1,7 +1,7 @@
 import { sequelizeConfig } from "../config";
 const { DataTypes } = require('sequelize');
 
-const Character =  sequelizeConfig.define('Character', {
+const Character = sequelizeConfig.define('Character', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
@@ -10,37 +10,26 @@ const Character =  sequelizeConfig.define('Character', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    comment: 'Nombre del personaje'
   },
   status: {
-    type: DataTypes.ENUM('Alive', 'Dead', 'unknown'),
+    type: DataTypes.ENUM('Alive', 'Dead', 'Unknown'),
     allowNull: false,
-    comment: 'Estado vital del personaje'
   },
   species: {
     type: DataTypes.STRING,
     allowNull: false,
-    comment: 'Especie del personaje'
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    comment: 'Tipo o subtipo del personaje'
   },
   gender: {
     type: DataTypes.ENUM('Female', 'Male', 'Genderless', 'unknown'),
     allowNull: false,
-    comment: 'Género del personaje'
   },
   origin: {
     type: DataTypes.STRING,
     allowNull: false,
-    comment: 'Planeta de origen del personaje'
   },
   image: {
     type: DataTypes.STRING,
     allowNull: false,
-    comment: 'URL de la imagen del personaje'
   }
 }, {
   tableName: 'characters',
