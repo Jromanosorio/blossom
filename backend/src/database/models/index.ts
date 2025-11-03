4f31621bc736da31367757aff4a64c3024e19191
Jromanosorio <jandres.roman.08@gmail.com>
2025-11-03 12:33:35 -0500

    db relations

---
+++ backend/src/database/models/index.ts
@@ -0,0 +1,29 @@
+import { Character } from "./Character.model"
+import { Comment } from "./Comment.model"
+import { Favorite } from "./Favorite.model"
+
+Character.hasOne(Favorite, {
+  foreignKey: 'characterId',
+  as: 'isFavorite',
+})
+
+Character.hasMany(Comment, {
+  foreignKey: 'characterId',
+  as: 'comments',
+})
+
+Comment.belongsTo(Character, {
+  foreignKey: 'characterId',
+  as: 'character',
+})
+
+Favorite.belongsTo(Character, {
+  foreignKey: 'characterId',
+  as: 'character',
+})
+
+export {
+    Character,
+    Favorite,
+    Comment
+}
